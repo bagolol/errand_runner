59a07e3a39f4cad9f28ba4263ab90baf735e0ace
Rocco <roccorighi@hotmail.com>
2015-07-02 17:13:53 +0100

    adds user serialiser to cutomize the json output

--- spec/factories/tasks.rb
+++ spec/factories/tasks.rb
@@ -1,10 +1,11 @@
 FactoryGirl.define do
   factory :task do
     title "MyString"
-lat "9.99"
-lon "9.99"
-description "MyString"
-open false
+  description "MyString"
+  lon 1
+  lat 1
+  open true
+  user
   end
 
 end
