63dfd72b302e69cc3bcfcbb26a8b4020956b5061
Mollie Stephenson <mollie.c.stephenson@gmail.com>
2015-07-05 13:30:10 +0100

    adds login homepage

--- front-end/www/js/autocompleteController.js
+++ front-end/www/js/autocompleteController.js
@@ -8,57 +8,57 @@ var componentForm = {
   postal_code: 'short_name'
 };
 
-  ionic.Platform.ready(function(){
-    autocomplete = new google.maps.places.Autocomplete(
-      /** @type {HTMLInputElement} */
-      (document.getElementById('autocomplete')), {
-        types: ['geocode']
-      })
-     google.maps.event.addListener(autocomplete, 'place_changed', function() {
-      fillInAddress();
-    })
-  })
+  // ionic.Platform.ready(function(){
+  //   autocomplete = new google.maps.places.Autocomplete(
+  //     /** @type {HTMLInputElement} */
+  //     (document.getElementById('autocomplete')), {
+  //       types: ['geocode']
+  //     })
+  //    google.maps.event.addListener(autocomplete, 'place_changed', function() {
+  //     fillInAddress();
+  //   })
+  // })
 
-  $scope.geolocate = function() {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(function(position) {
-        var geolocation = new google.maps.LatLng(
-          position.coords.latitude, position.coords.longitude);
-        var circle = new google.maps.Circle({
-          center: geolocation,
-          radius: position.coords.accuracy
-        });
-        // autocomplete.setBounds(circle.getBounds());
-      });
-    }
-  }
+  // $scope.geolocate = function() {
+  //   if (navigator.geolocation) {
+  //     navigator.geolocation.getCurrentPosition(function(position) {
+  //       var geolocation = new google.maps.LatLng(
+  //         position.coords.latitude, position.coords.longitude);
+  //       var circle = new google.maps.Circle({
+  //         center: geolocation,
+  //         radius: position.coords.accuracy
+  //       });
+  //       // autocomplete.setBounds(circle.getBounds());
+  //     });
+  //   }
+  // }
 
-  function fillInAddress() {
-    // Get the place details from the autocomplete object.
-    var place = autocomplete.getPlace();
+  // function fillInAddress() {
+  //   // Get the place details from the autocomplete object.
+  //   var place = autocomplete.getPlace();
 
-    for (var component in componentForm) {
-      document.getElementById(component).value = '';
-      document.getElementById(component).disabled = false;
-    }
-    console.log(place.address_components)
-      // Get each component of the address from the place details
-      // and fill the corresponding field on the form.
-    for (var i = 0; i < place.address_components.length; i++) {
-      var addressType = place.address_components[i].types[0];
-      if (componentForm[addressType]) {
-        var val = place.address_components[i][componentForm[addressType]];
-        document.getElementById(addressType).value = val;
-      }
-    }
-  }
+  //   for (var component in componentForm) {
+  //     document.getElementById(component).value = '';
+  //     document.getElementById(component).disabled = false;
+  //   }
+  //   console.log(place.address_components)
+  //     // Get each component of the address from the place details
+  //     // and fill the corresponding field on the form.
+  //   for (var i = 0; i < place.address_components.length; i++) {
+  //     var addressType = place.address_components[i].types[0];
+  //     if (componentForm[addressType]) {
+  //       var val = place.address_components[i][componentForm[addressType]];
+  //       document.getElementById(addressType).value = val;
+  //     }
+  //   }
+  // }
 
   $scope.codeAddress = function() {
     var stnumber = document.getElementById('street_number').value
     var route = document.getElementById('route').value
-    var city = document.getElementById('city').value
+    var city = document.getElementById('locality').value
     var postcode = document.getElementById('postal_code').value
-    var address = stnumber + ' ' + route + ' ' + country + ' ' + postcode
+    var address = stnumber + ' ' + route + ' ' + city + ' ' + postcode
     console.log(address)
     var geocoder = new google.maps.Geocoder();
     geocoder.geocode({
