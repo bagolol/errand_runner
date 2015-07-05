716e4e53954d7f26f23308cb35a4b1308ba10130
Anna <anna.klimas@gmail.com>
2015-07-05 14:14:04 +0100

    Autocomplete working

--- front-end/www/js/autocompleteController.js
+++ front-end/www/js/autocompleteController.js
@@ -8,50 +8,7 @@ var componentForm = {
   postal_code: 'short_name'
 };
 
-  // ionic.Platform.ready(function(){
-  //   autocomplete = new google.maps.places.Autocomplete(
-  //     /** @type {HTMLInputElement} */
-  //     (document.getElementById('autocomplete')), {
-  //       types: ['geocode']
-  //     })
-  //    google.maps.event.addListener(autocomplete, 'place_changed', function() {
-  //     fillInAddress();
-  //   })
-  // })
 
-  // $scope.geolocate = function() {
-  //   if (navigator.geolocation) {
-  //     navigator.geolocation.getCurrentPosition(function(position) {
-  //       var geolocation = new google.maps.LatLng(
-  //         position.coords.latitude, position.coords.longitude);
-  //       var circle = new google.maps.Circle({
-  //         center: geolocation,
-  //         radius: position.coords.accuracy
-  //       });
-  //       // autocomplete.setBounds(circle.getBounds());
-  //     });
-  //   }
-  // }
-
-  // function fillInAddress() {
-  //   // Get the place details from the autocomplete object.
-  //   var place = autocomplete.getPlace();
-
-  //   for (var component in componentForm) {
-  //     document.getElementById(component).value = '';
-  //     document.getElementById(component).disabled = false;
-  //   }
-  //   console.log(place.address_components)
-  //     // Get each component of the address from the place details
-  //     // and fill the corresponding field on the form.
-  //   for (var i = 0; i < place.address_components.length; i++) {
-  //     var addressType = place.address_components[i].types[0];
-  //     if (componentForm[addressType]) {
-  //       var val = place.address_components[i][componentForm[addressType]];
-  //       document.getElementById(addressType).value = val;
-  //     }
-  //   }
-  // }
 
   $scope.codeAddress = function() {
     var stnumber = document.getElementById('street_number').value
@@ -83,5 +40,50 @@ var componentForm = {
   $scope.taskData = {}
   $scope.newTask = function() {
     console.log($scope.taskData)
+  }
+
+    ionic.Platform.ready(function(){
+    autocomplete = new google.maps.places.Autocomplete(
+      /** @type {HTMLInputElement} */
+      (document.getElementById('autocomplete')), {
+        types: ['geocode']
+      })
+     google.maps.event.addListener(autocomplete, 'place_changed', function() {
+      fillInAddress();
+    })
+  })
+
+  $scope.geolocate = function() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var geolocation = new google.maps.LatLng(
+          position.coords.latitude, position.coords.longitude);
+        var circle = new google.maps.Circle({
+          center: geolocation,
+          radius: position.coords.accuracy
+        });
+        // autocomplete.setBounds(circle.getBounds());
+      });
+    }
+  }
+
+  function fillInAddress() {
+    // Get the place details from the autocomplete object.
+    var place = autocomplete.getPlace();
+
+    for (var component in componentForm) {
+      document.getElementById(component).value = '';
+      document.getElementById(component).disabled = false;
+    }
+    console.log(place.address_components)
+      // Get each component of the address from the place details
+      // and fill the corresponding field on the form.
+    for (var i = 0; i < place.address_components.length; i++) {
+      var addressType = place.address_components[i].types[0];
+      if (componentForm[addressType]) {
+        var val = place.address_components[i][componentForm[addressType]];
+        document.getElementById(addressType).value = val;
+      }
+    }
   }
 })
