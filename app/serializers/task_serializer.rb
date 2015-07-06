a3539bc4286122afa5a15ed0ecb34ce6f90c0e09
Joanne Zanetton <joannezanetton@gmail.com>
2015-07-06 18:12:13 +0100

    Amends pick-up and drop-off address, lat & lon

--- app/serializers/task_serializer.rb
+++ app/serializers/task_serializer.rb
@@ -1,4 +1,4 @@
 class TaskSerializer < ActiveModel::Serializer
-  attributes :id, :title, :description, :open, :lat, :lon, :user_id
+  attributes :id, :title, :description, :open, :pick_up_lat, :pick_up_lon, :pick_up_address, :drop_off_lat, :drop_off_lon, :drop_off_address, :user_id
   has_one :user
 end
